2f6be1aeeb87c0bc7a500c2c3c9faf063b0762a9
kimseokyung <traeumen97@naver.com>
2021-12-16 17:12:49 +0900

    weather API

---
+++ weather/js/quote.js
@@ -0,0 +1,49 @@
+const quotes = [
+{
+    quote: "Life is what happens when you’re busy making other plans.",
+    author: "John Lennon",
+},
+{
+    quote: "When the going gets tough, the tough get going.",
+    author: "Joe Kennedy",
+},
+{
+    quote: "You must be the change you wish to see in the world.",
+    author: "Mahatma Gandhi",
+},
+{
+    quote: "You only live once, but if you do it right, once is enough.",
+    author: "Mae West",
+},
+{
+    quote: "Tough times never last but tough people do.",
+    author: "Robert H. Schuller",
+},
+{
+    quote: "Get busy living or get busy dying.",
+    author: "Stephen King",
+},
+{
+    quote: "Whether you think you can or you think you can’t, you’re right.",
+    author: "Henry Ford",
+},
+{
+    quote: "Tis better to have loved and lost than to have never loved at all.",
+    author: "Alrded Lord Tennyson",
+},
+{
+    quote: "A man is but what he knows.",
+    author: "Sir Francis Bacon",
+},
+{
+    quote: "You miss 100 percent of the shots you never take.",
+    author: "Wayne Gretzky",
+},
+]
+const quote = document.querySelector("#quote span:first-child");
+const author = document.querySelector("#quote span:last-child");
+
+const todaysQuote = quotes[Math.floor(Math.random() * quotes.length)];
+
+quote.innerText = todaysQuote.quote;
+author.innerText = todaysQuote.author;
