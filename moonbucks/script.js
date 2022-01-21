867c8aac3680988796a08e3b0a9c07634e15783b
김서경 <traeumen97@naver.com>
2022-01-21 20:56:06 +0900

    경로/이름 수정

--- moonbucks/script.js
+++
@@ -1,8 +0,0 @@
-function App() {
-  document.querySelector("#espresso-menu-name").addEventListener("keypress", (e) => {
-    console.log(e.key);
-    if (e.key === 'Enter') {
-      console.log(document.querySelector("#espresso-menu-name").value);
-    }
-  });
-}
