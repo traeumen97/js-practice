325f6d0de73af874f7585e0d0f4c811d6ca0343f
kimseokyung <traeumen97@naver.com>
2022-02-08 23:55:55 +0900

    espresso name  삭제

--- moonbucks/js/script.js
+++ moonbucks/js/script.js
@@ -49,27 +49,27 @@ function App() {
       </li> `;
     }).join("");
       
-        $("#espresso-menu-list").innerHTML = template;
+        $("#menu-list").innerHTML = template;
         // const li 갯수를 카운팅
         updateMenuCount();
   };
 
   //총 메뉴 갯수 카운트
   const updateMenuCount = () => {
-    const memuCount = $("#espresso-menu-list").querySelectorAll("li").length;
+    const memuCount = $("#menu-list").querySelectorAll("li").length;
     $(".menu-count").innerText = `총 ${memuCount} 개`;
   };
 
   const addMenuName = () => {
-    if ($("#espresso-menu-name").value === "") {
+    if ($("#menu-name").value === "") {
       alert("값을 입력해주세요.");
       return;
     }
-    const espressoMenuName = $("#espresso-menu-name").value;
+    const espressoMenuName = $("#menu-name").value;
     this.menu[this.currentCategory].push({ name: espressoMenuName });
     store.setLocalStorage(this.menu);
     render();
-    $("#espresso-menu-name").value = "";
+    $("#menu-name").value = "";
   };
 
   const updateMenuName = (e) => {
@@ -92,7 +92,7 @@ function App() {
   };
 
   //메뉴 수정 버튼
-  $("#espresso-menu-list").addEventListener("click", (e) => {
+  $("#menu-list").addEventListener("click", (e) => {
     if(e.target.classList.contains("menu-edit-button")) {
       updateMenuName(e);
     }
@@ -103,15 +103,15 @@ function App() {
   });    
 
   //form태그가 자동으로 전송되는걸 막아줌
-  $("#espresso-menu-form").addEventListener("submit", (e) => {
+  $("#menu-form").addEventListener("submit", (e) => {
     e.preventDefault();
   });
 
-  // $("#espresso-menu-submit-button").addEventListener("click", () => {addMenuName();});
-  $("#espresso-menu-submit-button").addEventListener("click", addMenuName);
+  // $("#menu-submit-button").addEventListener("click", () => {addMenuName();});
+  $("#menu-submit-button").addEventListener("click", addMenuName);
 
   //메뉴의 이름일 입력받는 건
-  $("#espresso-menu-name").addEventListener("keypress", (e) => {
+  $("#menu-name").addEventListener("keypress", (e) => {
     if (e.key !== "Enter") {
       return;
     }
@@ -122,7 +122,9 @@ function App() {
     const isCategoryButton = e.target.classList.contains("cafe-category-name");
     if (isCategoryButton) {
       const categoryName = e.target.dataset.categoryName;
-      console.log(categoryName);
+      this.currentCategory = categoryName;
+      $("#category-title").innerText = `${e.target.innerText} 메뉴 관리`;
+      render();
     }
   });
 }
