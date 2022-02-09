957e6067c1135c61dac51f6f4ba8f52eec8ab68e
kimseokyung <traeumen97@naver.com>
2022-02-09 23:19:41 +0900

    품절상태관리 추가 완료

--- moonbucks/js/script.js
+++ moonbucks/js/script.js
@@ -31,21 +31,27 @@ function App() {
     const template = this.menu[this.currentCategory].map((menuItem, index) => {
       return `
       <li data-menu-id="${index}" class="menu-list-item d-flex items-center py-2">
-      <span class="w-100 pl-2 menu-name">${menuItem.name}</span>
-      <div class="menu-btn-warp">
-        <button
-          type="button"
-          class="bg-gray-50 text-gray-500 text-sm mr-1 menu-edit-button"
-        >
-          수정
-        </button>
-        <button
-          type="button"
-          class="bg-gray-50 text-gray-500 text-sm menu-remove-button"
-        >
-          삭제
-        </button>
-      </div>
+        <span class="w-100 pl-2 menu-name ${menuItem.soldOut ? "sold-out" : ""} ">${menuItem.name}</span>
+        <div class="menu-btn-warp">
+          <button
+            type="button"
+            class="bg-gray-50 text-gray-500 text-sm mr-1 menu-sold-out-button"
+          >
+            품절
+          </button>
+          <button
+            type="button"
+            class="bg-gray-50 text-gray-500 text-sm mr-1 menu-edit-button"
+          >
+            수정
+          </button>
+          <button
+            type="button"
+            class="bg-gray-50 text-gray-500 text-sm menu-remove-button"
+          >
+            삭제
+          </button>
+        </div>
       </li> `;
     }).join("");
       
@@ -90,15 +96,29 @@ function App() {
       updateMenuCount();
     }
   };
+  
+  const soldOutMenu = (e) => {
+    const menuId = e.target.closest("li").dataset.menuId;
+    this.menu[this.currentCategory][menuId].soldOut = !this.menu[this.currentCategory][menuId].soldOut;
+    store.setLocalStorage(this.menu);
+    render();
+  };
 
   //메뉴 수정 버튼
   $("#menu-list").addEventListener("click", (e) => {
     if(e.target.classList.contains("menu-edit-button")) {
       updateMenuName(e);
+      return;
     }
     //메뉴 삭제 버튼
     if(e.target.classList.contains("menu-remove-button")) {
       removeMenuName(e);
+      return;
+    }
+    
+    if (e.target.classList.contains("menu-sold-out-button")) {
+      soldOutMenu(e);
+      return;
     }
   });    
 
